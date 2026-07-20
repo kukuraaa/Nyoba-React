c7799f046a2314c54e73997da8b66177fb02c300
sitikhumaera <sitikhumaera.1601@gmail.com>
2026-07-20 13:21:31 +0700

    percobaan React pertama

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+
+ const Header = () => {
+  return (
+    <div>
+      <h1 id='header_id' className='header'>Hello</h1>
+       <p>Nama Lengkap: Khumaera</p>
+      <p>Divisi: Frontend Developer</p>
+    </div>
+  )
+
+
+  
+}
+export default Header

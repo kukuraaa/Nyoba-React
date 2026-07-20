0004b0cabff3655121ec22dc2a98660a72c5be00
sitikhumaera <sitikhumaera.1601@gmail.com>
2026-07-20 14:12:04 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import "./App.css";
+import foto from "./assets/ra.jpeg";
 
-import './App.css'
-
- const Header = () => {
+function App() {
   return (
-    <div>
-      <h1 id='header_id' className='header'>Hello</h1>
-       <p>Nama Lengkap: Khumaera</p>
-      <p>Divisi: Frontend Developer</p>
+    <div className="container">
+      <nav className="navbar">
+        <a href="#profil">Profil</a>
+        <a href="#motivasi">Motivasi</a>
+        <a href="#footer">Footer</a>
+      </nav>
+
+      <div className="profil" id="profil">
+        <div className="foto">
+          <img src={foto} alt="Foto" />
+        </div>
+
+       <div className="biodata">
+        <p>Nama : Khumaera</p>
+        <p>Sekolah : SMKN 2 Sukabumi</p>
+        <p>Jurusan : RPL</p>
+      </div>
+      </div>
+
+      <div className="motivasi" id="motivasi">
+        <h2>Motivasi Ingin Menjadi Frontend Developer</h2>
+
+        <p>
+          Saya ingin menjadi Frontend Developer karena saya tertarik membuat
+          tampilan website yang menarik, dan juga ingin belajar lebih dalam
+          lagi di bidang Frontend Development.
+        </p>
+      </div>
+
+      <footer id="footer">
+        <p className="copyright">
+          © 2026 Homieraa
+        </p>
+      </footer>
+
     </div>
-  )
-
-
-  
+  );
 }
-export default Header
+
+export default App;
